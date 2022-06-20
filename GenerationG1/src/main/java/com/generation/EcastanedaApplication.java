3c585332a652e5a2125f6ab81e48a305349a88d3
Esancal <esteban.ca2000@outlook.cl>
2022-06-20 13:09:10 -0400

    Añadido el proyecto GenerationG1

---
+++ GenerationG1/src/main/java/com/generation/EcastanedaApplication.java
@@ -0,0 +1,13 @@
+package com.generation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcastanedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcastanedaApplication.class, args);
+	}
+
+}
---
+++ GenerationG1/src/test/java/com/generation/ecastaneda/EcastanedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.ecastaneda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcastanedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
